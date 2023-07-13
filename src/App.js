a09c0d42971da8b697bc60a844e678b76ce76315
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-07-13 14:18:41 +0100

    fixing the routrer essues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
 function App() {
 return (
     <div className="App">
