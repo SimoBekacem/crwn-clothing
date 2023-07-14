103d5c018db47fb8ba6e1d07802d84df07f269d8
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-07-14 09:54:16 +0100

    making the routing from and to hatspage works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Switch , Route  } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
-function App() {
+import HatsPage from './pages/hatspage/hatspage.component';
+
+
+
+function App(props) {
 return (
     <div className="App">
-        <HomePage />
+        <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/hats' component={HatsPage} />
+        </Switch>
+        
     </div>
 );
 }
