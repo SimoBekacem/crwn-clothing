d7bc89072ff544536d629f9ca70d5f3bfa7c5b5f
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-07-15 09:37:30 +0100

    making the shop page with there component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Switch , Route  } from 'react-router-dom';
+import './app.style.scss';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 
 
 function App(props) {
 return (
     <div className="App">
+        <Header />
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route exact path='/shop' component={ShopPage} />
