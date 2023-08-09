215e4cf0c5c089fbb9b3114fe6b1adc65348981e
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-08-09 20:54:12 +0100

    use the redux-presist for the localstorage

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
 
 import App from './App';
 import store from './redux/store';
 
-
+let persistor = persistStore(store)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
     </Provider>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
 
 import  userReducer  from "./slices/user-slice";
 import cardReducer from './slices/card-slice';
 
+const persistConfig = {
+    key:'root',
+    version: 1,
+    storage
+};
+
+const reducer = combineReducers({
+    user: userReducer,
+    card: cardReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 export default configureStore ({
-    reducer:{
-        user: userReducer,
-        card: cardReducer
-    }
+    reducer:persistedReducer
 })
