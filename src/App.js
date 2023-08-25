80dd728d8f5c6731dd62fd489478c441f7f1993a
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-08-25 20:19:09 +0100

    start using the lazy and suspense for upgrading the perfomence of the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -6,11 +6,12 @@ import './app.style.scss';
 
 import UserContext from './contexts/user/user-context';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shoppage/shoppage.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Checkout from './pages/checkoutpage/checkout-component';
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shoppage/shoppage.component'));
+const Header = lazy(() => import('./components/header/header.component'));
+const SignInAndSignUp = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const Checkout = lazy(() => import('./pages/checkoutpage/checkout-component'));
+
 const App = () => {
   const [curranteUser, setcurranteUser] = useState({
     user: undefined,
@@ -32,6 +33,7 @@ const App = () => {
     });
     unsubscribeFromAuth();
   }, []);
+  // * this useEffect for work with redux  but you should call the useDispatch and the useSelector:
   // useEffect(()=>{
   //     const unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
   //         if (userAuth){
@@ -49,24 +51,26 @@ const App = () => {
   // },[])
   return (
     <div className="App">
-      <UserContext.Provider value={curranteUser.user}>
-        <Header />
-      </UserContext.Provider>
-      <Routes>
-        <Route path="/crwn-clothing" element={<HomePage />} />
-        <Route path="/crwn-clothing/shop/*" element={<ShopPage />} />
-        <Route
-          path="/crwn-clothing/sign-in"
-          element={
-            curranteUser.user ? (
-              <Navigate to="/crwn-clothing" />
-            ) : (
-              <SignInAndSignUp />
-            )
-          }
-        />
-        <Route path="/crwn-clothing/checkout" element={<Checkout />} />
-      </Routes>
+      <Suspense fallback={<div>...loading</div>}>
+        <UserContext.Provider value={curranteUser.user}>
+          <Header />
+        </UserContext.Provider>
+        <Routes>
+          <Route path="/crwn-clothing" element={<HomePage />} />
+          <Route path="/crwn-clothing/shop/*" element={<ShopPage />} />
+          <Route
+            path="/crwn-clothing/sign-in"
+            element={
+              curranteUser.user ? (
+                <Navigate to="/crwn-clothing" />
+              ) : (
+                <SignInAndSignUp />
+              )
+            }
+          />
+          <Route path="/crwn-clothing/checkout" element={<Checkout />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
